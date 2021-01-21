92ce549cc5ccad0571a5632dbf113898625624de
batuhankrbb <batuhankarababa@gmail.com>
2021-01-21 10:10:17 +0300

    Initial Commit

---
+++ InAppTesting/InAppTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppTestingApp.swift
+//  InAppTesting
+//
+//  Created by Batuhan Karababa on 21.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct InAppTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
