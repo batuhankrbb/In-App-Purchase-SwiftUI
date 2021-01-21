eaf8f47c16272e18523efd26d745e7dd9bf644f7
batuhankrbb <batuhankarababa@gmail.com>
2021-01-21 12:49:09 +0300

    dk 38.40

--- InAppTesting/ContentView.swift
+++ InAppTesting/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @EnvironmentObject var store:StoreService
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(store.allRecipies, id: \.self){recipe in
+                NavigationLink(
+                    destination: Text("test"),
+                    label: {
+                        RecipeCell(recipe: recipe) {
+                            
+                        }
+                    })
+            }
+        }
     }
 }
 
@@ -21,4 +29,47 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct RecipeCell:View{
+    let recipe:Recipe
+    let action: () -> Void
+    
+    var body: some View{
+        HStack{
+            ZStack{
+                Image(recipe.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80)
+                    .cornerRadius(10)
+                    .opacity(recipe.isLocked ? 0.8 : 1)
+                    .blur(radius: recipe.isLocked ? 3 : 0)
+                    .padding()
+                
+                Image(systemName: "lock.fill")
+                    .font(.largeTitle)
+                    .opacity(recipe.isLocked ? 1 : 0)
+            }
+            VStack{
+                Text(recipe.title)
+                    .font(.title)
+                Text(recipe.description)
+                    .font(.caption)
+            }
+            Spacer()
+            if let price = recipe.price, recipe.isLocked{
+                Button(action: {
+                    
+                }){
+                    Text(price)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(25)
+                }
+            }
+        }
+    }
+    
 }
